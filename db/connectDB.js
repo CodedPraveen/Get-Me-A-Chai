5669040f29ed07544cfb0565248e9cde241b71dc
Project <praveenbishnoi8809@gmail.com>
2026-03-12 12:14:10 +0530

    Bug solve 'DNS Error' and log comment out

--- db/connectDB.js
+++ db/connectDB.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        // console.log(`MongoDB Connected: ${conn.connection.host}`);
 
     } catch (error) {
         console.error("MongoDB Error:", error);
